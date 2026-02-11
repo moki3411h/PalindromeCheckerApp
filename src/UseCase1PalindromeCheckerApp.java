1ebb6bb225926c05663636392b2da6db2ed781d9
moki3411h <mokesh7319@gmail.com>
2026-02-11 15:58:05 +0530

    first commit

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("\n Welcome to palindrome");
+        System.out.println("its not ");
+
+    }
+}
