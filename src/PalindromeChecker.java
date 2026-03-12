a0752a306deed5e307123839640a567dffd612a8
moki3411 <mokesh7319@gmail.com>
2026-03-12 15:24:39 +0530

    UC5: Stack-Based Palindrome Checker

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,23 +1,33 @@
-public class PalindromeChecker {
-    public static void main(String[]  args){
+import java.util.Stack;
+
+class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
         String word = "madam";
-        char[] characters = word.toCharArray();
-        int start = 0;
-        int end = characters.length -1;
-        boolean ispalindrome = true;
-        while (start < end){
-        if(characters[start] != characters[end]){
-              ispalindrome = false;
-              break;
+
+        // Create a stack
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character into the stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
-        start++;
-        end--;
+
+        // Build reversed string by popping from stack
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
         }
-        if (ispalindrome) {
+
+        // Compare original and reversed
+        if (word.equals(reversed)) {
             System.out.println("The string '" + word + "' is a palindrome.");
-        }else{
-            System.out.println("The string '" + word + "' is not palindrome.");
-        }
+        } else {
+            System.out.println("The string '" + word + "' is not a palindrome.");
         }
 
     }
+}
